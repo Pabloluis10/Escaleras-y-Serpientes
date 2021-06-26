095d31299121ccb60b0b83c0ed5d815d890c3ac8
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-25 21:36:30 -0600

    Agregue el VectorJugadores.java el metodo infoJugadores

--- src/main/java/escalerasyserpientes/jugadores/VectorJugadores.java
+++ src/main/java/escalerasyserpientes/jugadores/VectorJugadores.java
@@ -1,6 +1,7 @@
 package escalerasyserpientes.jugadores;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -8,9 +9,24 @@ import java.util.ArrayList;
  */
 public class VectorJugadores {
     //atributos
-    private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+    private ArrayList<Jugador> jugadores = new ArrayList<>();
+    private Scanner entrada = new Scanner(System.in);
 
     public VectorJugadores() {
+    }
+    
+    public void ingresarUsuario(int id, String nombre, String apellido){
+        jugadores.add(new Jugador(id, nombre, apellido));
+    }
+    
+    public ArrayList<String> infoJugadores(){
+        ArrayList<String> info = new ArrayList<>();
+        
+        for(int i=0; i<jugadores.size(); i++){
+            info.add(jugadores.get(i).toString());
+        }
+  
+        return info;
     }
     
     
